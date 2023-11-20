b2cc3a011a0d9c2e273e6b1f817a32d1356b9310
Will1404 <123519935+Will1404@users.noreply.github.com>
2023-11-19 22:23:57 -0600

    Codigo listas
    Se adiciona el codigo con el codigo de multiplicacion de listas

--- Tarea GitHub.py
+++ Tarea GitHub.py
@@ -1,7 +1,14 @@
 #Tarea extra 3
 
 lista=[1,2,3,4,5,6,7,8,9,10]
-total=0
+
+#Primera parte de la suma
+#total=0
+#for i in lista:
+#    total=i+total
+#print(total)
+
+total=1
 for i in lista:
-    total=i+total
+    total=i*total
 print(total)
